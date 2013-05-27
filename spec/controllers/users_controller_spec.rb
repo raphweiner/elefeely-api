567a5c6a219e5cd22c80c11357367c2a47445860
Raphael Weiner <raphael.weiner@gmail.com>
2013-05-26 23:22:24 -0600

    user, reset pw mailer, sorcery setup

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
-describe UsersController do
-  describe 'POST #create' do
-    context 'happy path' do
-      let(:params) do
-        { user: { email: 'raphael@example.com',
-                  password: 'password' } }
-      end
-    end
+# describe UsersController do
+#   describe 'POST #create' do
+#     context 'happy path' do
+#       let(:params) do
+#         { user: { email: 'raphael@example.com',
+#                   password: 'password' } }
+#       end
+#     end
 
-    context 'sad path' do
-    end
-  end
-end
+#     context 'sad path' do
+#     end
+#   end
+# end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    User.new(email: 'rafi@example.com', password: 'omg!')
+  end
+
+  it 'requires an email' do
+    expect { subject.email = nil }.to change { subject.valid? }.to false
+  end
+
+  it 'requires a unique email' do
+    expect { User.create(email: 'rafi@example.com', password: 'yup') }
+      .to change { subject.valid? }.to false
+  end
+
+  it 'requires a password' do
+    expect { subject.password = nil }.to change { subject.valid? }.to false
+  end
 end
