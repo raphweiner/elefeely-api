eb209b06b8fb09decc3852badcce64521dc706b2
Raphael Weiner <raphael.weiner@gmail.com>
2013-06-02 20:59:46 -0600

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,20 +8,23 @@ me = User.where(email: "phyzikz@gmail.com").first ||
          User.create(email: "phyzikz@gmail.com", password: "password")
 
 
-create_feelings(100, user)
-create_feelings(10, me)
+module Seed
+  def self.create_feelings(count, user, source)
+    oldest_time = Time.local(2013, 1, 1)
+    newest_time = Time.now
 
-def create_feelings(count, user)
-  oldest_time = Time.local(2013, 1, 1)
-  newest_time = Time.now
+    (1..count).each do |_|
+      created_at = Time.at(oldest_time + rand * (newest_time.to_f - oldest_time.to_f))
 
-  (1..count).each do |_|
-    created_at = Time.at(oldest_time + rand * (newest_time.to_f - oldest_time.to_f))
-
-    feeling = user.feelings.build(source: source,
-                                  source_event_id: rand(1000000),
-                                  score: rand(5) + 1)
-    feeling.created_at = created_at
-    feeling.save
+      feeling = user.feelings.build(source: source,
+                                    source_event_id: rand(1000000),
+                                    score: rand(5) + 1)
+      feeling.created_at = created_at
+      feeling.save
+    end
   end
 end
+
+Seed.create_feelings(100, user, source)
+Seed.create_feelings(10, me, source)
+
